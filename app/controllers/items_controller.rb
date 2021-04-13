62a82737fa29a92264250b39b5b67cc619297ecd
kawamatatakuya <105x7b43a12fba1y@gmail.com>
2021-04-13 17:56:58 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
+    before_action :authenticate_user!,  except: [:show, :index]
     def index
-        
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+          redirect_to root_path
+        else
+          render :new
+        end
+    end
+
+    def edit
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:product, :product_description, :category_id, :status_id, :delivery_fee_id, :shipment_street_id, :shipment_day_id,  :price, :image).merge(user_id: current_user.id)
     end
 end

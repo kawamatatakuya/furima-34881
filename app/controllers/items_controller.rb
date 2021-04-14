e4aea0567feb8f3fe8747a83a70b3b17d27a6bc6
kawamatatakuya <105x7b43a12fba1y@gmail.com>
2021-04-14 21:14:43 +0900

    商品編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!,  except: [:show, :index]
+    before_action :move_to_login,  except: [:show, :index]
+    
     def index
       @items = Item.all
     end
@@ -19,16 +21,31 @@ class ItemsController < ApplicationController
 
     def show
       @item = Item.find(params[:id])
-      # @item = Item.all
-      # @item = Item.find(item_params)
     end
 
-    # def edit
-    # end
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+         redirect_to item_path
+      else
+        render :edit
+      end
+  end
 
     private
 
     def item_params
         params.require(:item).permit(:product, :product_description, :category_id, :status_id, :delivery_fee_id, :shipment_street_id, :shipment_day_id,  :price, :image).merge(user_id: current_user.id)
+    end
+
+    def move_to_login
+      @item = Item.find(params[:id])
+      if current_user.id != @item.user_id
+        redirect_to root_path
+      end
     end
 end
